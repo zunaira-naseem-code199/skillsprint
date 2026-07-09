7e8094bf1964f766f6a31855c67e5865a5776b1a
Zunaira <znaseem100@gmail.com>
2026-07-09 13:23:38 +0500

    Updated frontend API URL for production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://skillsprint-production-4d66.up.railway.app/api",
 });
 
 export default API;
